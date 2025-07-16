eb624b38e982b17119ee97e184e98b7b73269a66
qiuye2024github <qiuye2024github@163.com>
2025-07-16 12:04:07 +0800

    init

---
+++ src/main/java/cn/qiuye/eaec/EMIAEC.java
@@ -0,0 +1,13 @@
+package cn.qiuye.eaec;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(EMIAEC.MODID)
+public class EMIAEC {
+
+    public static final String MODID = "emi_ae2_compatible";
+    public static final String NAME = "EMI AE2 Compatible";
+    public static final Logger LOGGER = LogManager.getLogger(NAME);
+}
